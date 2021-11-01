caa6e3196cd1fb2a3de813a70579143bc0d06d52
yiannisRoss <rossiadisj@gmail.com>
2021-11-01 11:44:09 +0200

    generate articles model, index view placeholder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_092844) do
+ActiveRecord::Schema.define(version: 2021_11_01_094002) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
