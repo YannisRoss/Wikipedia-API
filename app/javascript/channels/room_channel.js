807d5d3e168c635371d019340aaa30321aececf9
grimhamr <rossiadisj@gmail.com>
2022-02-11 13:51:10 +0200

    fix bug where search terms with 0 wiki entries would cause error, and would not show up on sidekiq live statistics as search terms

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -14,24 +14,40 @@ const roomChannel = consumer.subscriptions.create("RoomChannel", {
     // Called when there's incoming data on the websocket for this channel
     console.log(data)
     let statisticsDiv = document.getElementById("statistics-div")
-    statisticsDiv.innerHTML =    `
-    <ul>
+    let statistics = `<ul>
     <li>Entries count:  ${data['entries'].length} </li>
     <li>Search terms:  ${data['terms'].length} </li>   
+    `
+    console.log("let's examine data['entries']")
+    console.log(data['entries'])
+    console.log(`length is ${data['entries'].length}`)
+    if (data['entries'].length > 0) {
 
-    <li>Entry with the biggest wordcount:  ${data['wordcount_max_min'][0]}</li>
+      statistics = statistics + `
     
-    <li>Entry with the lowest wordcount:  ${data['wordcount_max_min'][1]}</li>
+      <li>Entry with the biggest wordcount:  ${data['wordcount_max_min'][0]}</li>
+      
+      <li>Entry with the lowest wordcount:  ${data['wordcount_max_min'][1]}</li>
+  
+      
+      <li>Entry with the most words in title:  ${data['biggest_title']} </li>
+      <li>Entry with the lowest levenshtein distance according to its search term (levenshtein value and search term):  ${data['lowest_levenshtein']} , searched from ${data['lowest_levenshtein_term']} </li>
+  
+  
+      </ul>
+        `
 
-    
-    <li>Entry with the most words in title:  ${data['biggest_title']} </li>
-    <li>Entry with the lowest levenshtein distance according to its search term (levenshtein value and search term):  ${data['lowest_levenshtein']} , searched from ${data['lowest_levenshtein_term']} </li>
+    }
+    else {
+      statistics = statistics + ` 
+      </ul>
+      `
+    }
 
 
-</ul>
-  `}
-},
-);
+    statisticsDiv.innerHTML = statistics
+  
+  }})
+  
 
-console.log('room_channel')
-
+  
--- app/jobs/statistics_broadcaster_job.rb
+++ app/jobs/statistics_broadcaster_job.rb
@@ -5,14 +5,34 @@ class StatisticsBroadcasterJob < ApplicationJob
     # Do something later
     puts 'sidekiq job started'
     wiki_entries = WikiEntry.all
-    ActionCable.server.broadcast 'room', {
+    search_term = SearchTerm.last
+    
+    statistics = {
       entries: wiki_entries, 
-      terms: SearchTerm.all, 
-      wordcount_max_min: [wiki_entries.order(:wordcount).limit(1).first.title, wiki_entries.order(wordcount: :desc).limit(1).first.title],
-      biggest_title: wiki_entries.sort_by{|entry| entry.title}.first.title,
-      lowest_levenshtein: wiki_entries.sort_by{|entry| entry.levenshtein_distance(entry.title, entry.search_term.body)}.first.title,
-      lowest_levenshtein_term: wiki_entries.sort_by{|entry| entry.levenshtein_distance(entry.title, entry.search_term.body)}.first.search_term.body,
-  
-  }
+      terms: SearchTerm.all,
+      wordcount_max_min: nil,
+      biggest_title: nil,
+      lowest_levenshtein: nil,
+      lowest_levenshtein_term: nil
+    }
+    
+    unless wiki_entries.empty? 
+
+      statistics[:wordcount_max_min] = wiki_entries.order(:wordcount).limit(1).first.title, wiki_entries.order(wordcount: :desc).limit(1).first.title
+      statistics[:biggest_title] = wiki_entries.sort_by{|entry| entry.title}.first.title
+      statistics[:lowest_levenshtein] = wiki_entries.sort_by{|entry| entry.levenshtein_distance(entry.title, entry.search_term.body)}.first.title
+      statistics[:lowest_levenshtein_term] = wiki_entries.sort_by{|entry| entry.levenshtein_distance(entry.title, entry.search_term.body)}.first.search_term.body
+
+    end
+
+  ActionCable.server.broadcast 'room', {
+    entries: statistics[:entries], 
+    terms: statistics[:terms], 
+    wordcount_max_min: statistics[:wordcount_max_min],
+    biggest_title: statistics[:biggest_title],
+    lowest_levenshtein: statistics[:lowest_levenshtein],
+    lowest_levenshtein_term: statistics[:lowest_levenshtein_term],
+
+}
   end
 end
