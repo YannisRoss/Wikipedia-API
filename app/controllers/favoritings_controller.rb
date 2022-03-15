e883557679524ef29273b297d7dd08278540949a
grimhamr <rossiadisj@gmail.com>
2022-03-15 17:13:42 +0200

    add redirect_back on favorite creation/deletion

--- app/controllers/favoritings_controller.rb
+++ app/controllers/favoritings_controller.rb
@@ -7,18 +7,18 @@ class FavoritingsController < ApplicationController
     @favoriting = Favoriting.new(user_id: current_user.id, wiki_entry_id: params[:id])
 
     if @favoriting.save
-      redirect_to root_path, notice: 'favoriting was successfully created.'
+      redirect_back fallback_location: root_path, notice: 'favoriting was successfully created.' 
     else
-      redirect_to root_path, alert: "favoriting failed, #{@favoriting.errors.full_messages.first}"
+      redirect_back fallback_location: root_path, alert: "favoriting failed, #{@favoriting.errors.full_messages.first}"
     end
   end
 
   def destroy
     @favoriting = Favoriting.where(user_id: current_user.id, wiki_entry_id: params[:id]).first
     if @favoriting.destroy
-      redirect_to root_path, notice: 'favoriting was successfully destroyed.'
+      redirect_back fallback_location: root_path, notice: 'favoriting was successfully destroyed.'
     else
-      redirect_to root_path, alert: "favoriting destroy failed, #{favoriting.errors.full_messages.first}"
+      redirect_back fallback_location: root_path, alert: "favoriting destroy failed, #{favoriting.errors.full_messages.first}"
     end
   end
 end
